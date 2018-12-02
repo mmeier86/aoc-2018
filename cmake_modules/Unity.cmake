c9c323a7c2a5ee967f1b2ddcb9284140cac5559f
Michael Meier <mmeier1986@googlemail.com>
2018-12-02 17:19:39 +0100

    Update Unity build
    Added handover of directory to UT execution.
    
    Added a call to enable_testing and fixed Unity path.

--- cmake_modules/Unity.cmake
+++ cmake_modules/Unity.cmake
@@ -7,11 +7,12 @@ option(UNITTESTS_ENABLED
   OFF
   )
 if(UNITTESTS_ENABLED)
+  enable_testing()
   add_custom_target(check
     COMMAND ${CMAKE_CTEST_COMMAND} -V
     )
 
-  set(UNITY_DIR ${CMAKE_CURRENT_LIST_DIR}/third_party/unity)
+  set(UNITY_DIR ${CMAKE_CURRENT_LIST_DIR}/../third_party/unity)
 
   add_library(unity STATIC
     ${UNITY_DIR}/src/unity.c
@@ -28,9 +29,12 @@ if(UNITTESTS_ENABLED)
   add_library(unity::unity ALIAS unity)
 
   macro(add_ut t_name t_file)
+    cmake_parse_arguments(ADDUT "" "T_DIR" "" ${ARGN})
     add_executable(${t_name} ${t_file})
     target_link_libraries(${t_name} PRIVATE unity::unity)
-    add_test(${t_name} ${t_name})
+    add_test(NAME ${t_name}
+      COMMAND ${t_name} ${ADDUT_T_DIR}
+      )
     add_dependencies(check ${t_name})
   endmacro()
   macro(link_ut t_name)
