16d9b8da1bb79ed37ce4e5afb93a2b2eb8957422
Michael Meier <mmeier1986@googlemail.com>
2018-12-27 21:52:02 +0100

    Add claims parser for day 03

--- day_03/cloth_cutting.c
+++ day_03/cloth_cutting.c
@@ -7,17 +7,44 @@
 
 #include "aoc_err.h"
 
+#include <stdbool.h>
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int parse_claim(char* in, claim_t* claim){
-  (void)(claim);
-  (void)(in);
-  return -1;
+  int res = sscanf(in, "#%*d @ %u,%u: %ux%u",
+                   &claim->startx,
+                   &claim->starty,
+                   &claim->lengthx,
+                   &claim->lengthy
+                   );
+  return res == 4 ? 0 : -1;
 }
 
 claim_t* parse_all_claims(tok_t* tok){
-  (void)(tok);
-  return NULL;
+  if(tok == NULL){
+    set_aoc_err_msg("Tokenizer is NULL.", 0);
+    return NULL;
+  }
+  claim_t* claims = malloc(sizeof(claim_t)*tok_count(tok));
+  char* curr_in = NULL;
+  claim_t* curr_claim = claims;
+  bool error = false;
+  while((curr_in = n_tok(tok)) != NULL){
+    if(parse_claim(curr_in, curr_claim++) != 0){
+      error = true;
+      break;
+    }
+  }
+  if(error){
+    char errmsg[64];
+    snprintf(errmsg, 64, "Error while parsing \"%s\".", curr_in);
+    set_aoc_err_msg(errmsg, 0);
+    free(claims);
+    return NULL;
+  }
+  return claims;
 }
 
 char* cloth_slicing(tok_t* tok){
--- day_03/test_day_03.c
+++ day_03/test_day_03.c
@@ -30,9 +30,63 @@ void test_part1_empty_tok_returns_null(void){
   free_tok(tok);
 }
 
+void test_parse_claims_null_sets_error(void){
+  claim_t* res = parse_all_claims(NULL);
+  TEST_ASSERT_NULL(res);
+  char* err = get_latest_aoc_err_msg();
+  TEST_ASSERT_EQUAL_STRING("Tokenizer is NULL.", err);
+  free(err);
+}
+
+void test_parse_claims_malformed_input_sets_error(void){
+  char in[] = "123 @ 3,2: 5r4";
+  tok_t* tok = get_tokenizer(in, "\n");
+  claim_t* res = parse_all_claims(tok);
+  TEST_ASSERT_NULL(res);
+  char* err = get_latest_aoc_err_msg();
+  TEST_ASSERT_EQUAL_STRING("Error while parsing \"123 @ 3,2: 5r4\".",err);
+  free(err);
+}
+
+void test_parse_claims_parse_single(void){
+  char in[] = "#123 @ 3,2: 5x4";
+  tok_t* tok = get_tokenizer(in, "\n");
+  claim_t* res = parse_all_claims(tok);
+  TEST_ASSERT_EQUAL_UINT(3u, res[0].startx);
+  TEST_ASSERT_EQUAL_UINT(2u, res[0].starty);
+  TEST_ASSERT_EQUAL_UINT(5u, res[0].lengthx);
+  TEST_ASSERT_EQUAL_UINT(4u, res[0].lengthy);
+  free(res);
+  free_tok(tok);
+}
+
+void test_parse_claims_aoc_example(void){
+  char in[] = "#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 5,5: 2x2";
+  tok_t* tok = get_tokenizer(in, "\n");
+  claim_t* res = parse_all_claims(tok);
+  TEST_ASSERT_EQUAL_UINT(1u, res[0].startx);
+  TEST_ASSERT_EQUAL_UINT(3u, res[0].starty);
+  TEST_ASSERT_EQUAL_UINT(4u, res[0].lengthx);
+  TEST_ASSERT_EQUAL_UINT(4u, res[0].lengthy);
+  TEST_ASSERT_EQUAL_UINT(3u, res[1].startx);
+  TEST_ASSERT_EQUAL_UINT(1u, res[1].starty);
+  TEST_ASSERT_EQUAL_UINT(4u, res[1].lengthx);
+  TEST_ASSERT_EQUAL_UINT(4u, res[1].lengthy);
+  TEST_ASSERT_EQUAL_UINT(5u, res[2].startx);
+  TEST_ASSERT_EQUAL_UINT(5u, res[2].starty);
+  TEST_ASSERT_EQUAL_UINT(2u, res[2].lengthx);
+  TEST_ASSERT_EQUAL_UINT(2u, res[2].lengthy);
+  free(res);
+  free_tok(tok);
+}
+
 int main(void){
   UNITY_BEGIN();
   RUN_TEST(test_part1_tok_null_returns_null);
   RUN_TEST(test_part1_empty_tok_returns_null);
+  RUN_TEST(test_parse_claims_null_sets_error);
+  RUN_TEST(test_parse_claims_malformed_input_sets_error);
+  RUN_TEST(test_parse_claims_parse_single);
+  RUN_TEST(test_parse_claims_aoc_example);
   return UNITY_END();
 }
